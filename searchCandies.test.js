5ef9fd1e9f3c178deb7333894b8db89742d2a366
Joshua Perrin <joshuaperrin101@gmail.com>
2022-09-15 11:57:53 +0100

    adding tests for search candies

--- searchCandies.test.js
+++ searchCandies.test.js
@@ -2,6 +2,22 @@ const searchCandies = require('./searchCandies')
 
 describe('searchCandies', () => {
     it('returns sweets starting Ma and under 10 pounds', () => {
-        expect(searchCandies('Ma', 10)).toBe(['Mars', 'Maltesars'])
+        expect(searchCandies('Ma', 10)).toEqual(['Mars', 'Maltesers'])
+    })
+
+    it('returns sweets starting Ma and under 2 pounds', () => {
+        expect(searchCandies('Ma', 2)).toEqual(['Mars'])
+    })
+
+    it('returns sweets starting S and under 10 pounds', () => {
+        expect(searchCandies('S', 10)).toEqual([ 'Skitties', 'Skittles', 'Starburst' ])
+    })
+
+    it('returns sweets starting S and under 4 pounds', () => {
+        expect(searchCandies('S', 4)).toEqual([ 'Skitties', 'Skittles' ] )
+    })
+
+    it('returns sweets starting Ma and under 10 pounds when lower case string passed', () => {
+        expect(searchCandies('ma', 10)).toEqual(['Mars', 'Maltesers'])
     })
 })
